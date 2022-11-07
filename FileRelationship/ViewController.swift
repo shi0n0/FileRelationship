c349a7922766822f9075d0bb8413fa540eed84c2
shi0n0 <shiono.yui1080@gmail.com>
2022-11-07 13:15:57 +0900

    Initial Commit

---
+++ FileRelationship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileRelationship
+//
+//  Created by 塩野　結 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
